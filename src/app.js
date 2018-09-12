7db9992af2e3b6db9bf58ca188d7c530edca1121
Stevan Popovic <stevanpopovic5@gmail.com>
2018-09-12 08:28:53 +0100

    Created index view with data outputting to page in div.

--- src/app.js
+++ src/app.js
@@ -14,15 +14,24 @@ class App extends React.Component {
       method: 'GET',
       url: '/api/shares/'
     })
-      .then(res => console.log(res))
-      .then(err => console.log(err));
+      .then(res => this.setState({ data: res.data }))
+      .catch(err => console.log('err', err));
   }
 
   render() {
-    // console.log(this.state);
+    console.log(this.state);
+    if(!this.state.data) return <h1>Loading...</h1>;
     return (
       <main>
         <h1>PROJECT_TITLE</h1>
+        {this.state.data.map(share =>
+          <div key={share.id}>
+            <h3>{share.title}</h3>
+            <h4>{share.user.username}</h4>
+            {share.likes.forEach(like => <p>{like}</p>)}
+            <img src={share.cover_url} />
+          </div>
+        )}
       </main>
     );
   }
