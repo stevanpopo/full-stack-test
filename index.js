d106a6d24bfe81d3ed7cf8639b942595394e334b
Stevan Popovic <stevanpopovic5@gmail.com>
2018-09-12 07:50:23 +0100

    Added router in main index.js.

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 
+const router = require('./config/router');
+
 app.use(express.static(`${__dirname}/public`));
+
+app.use('/api', router);
 
 app.listen(port, () => console.log(`Express running on port ${port}`));
 module.exports = app;
