30b88a77f70486ef6a5b58c38b7d154ae50b4ae8
Stevan Popovic <stevanpopovic5@gmail.com>
2018-09-12 08:41:59 +0100

    #1. Added Bulma for styling and responsiveness.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import axios from 'axios';
 // import './scss/main.scss';
 
+import 'bulma';
+
 class App extends React.Component {
   constructor(){
     super();
@@ -22,16 +24,35 @@ class App extends React.Component {
     console.log(this.state);
     if(!this.state.data) return <h1>Loading...</h1>;
     return (
-      <main>
-        <h1>PROJECT_TITLE</h1>
-        {this.state.data.map(share =>
-          <div key={share.id}>
-            <h3>{share.title}</h3>
-            <h4>{share.user.username}</h4>
-            {share.likes.forEach(like => <p>{like}</p>)}
-            <img src={share.cover_url} />
-          </div>
-        )}
+      <main className="container">
+        <h1 className="title is-1">PROJECT_TITLE</h1>
+        <div className="columns is-multiline">
+          {this.state.data.map(share =>
+
+            <div className="card column is-one-quarter-desktop is-one-third-tablet" key={share.id}>
+              <div className="card-image">
+                <figure className="image is-4by3">
+                  <img src={share.cover_url} alt="Placeholder image" />
+                </figure>
+              </div>
+              <div className="card-content">
+                <div className="media">
+                  <div className="media-content">
+                    <p className="title is-4">{share.title}</p>
+                    <p className="subtitle is-6">{share.user.username}</p>
+                    {share.likes.forEach(like => <p>{like}</p>)}
+                  </div>
+                </div>
+
+                <div className="content">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Phasellus nec iaculis mauris.
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+
       </main>
     );
   }
